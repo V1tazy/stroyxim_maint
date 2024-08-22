f541a5a6e0050502ca236d87f37c78ac84cedc53
NikitaChebotov <nikitachebotov@gmail.com>
2024-08-22 10:34:41 +0900

    Zalupapa

--- stroyximtorg.client/src/components/CallBack/Callback.jsx
+++ stroyximtorg.client/src/components/CallBack/Callback.jsx
@@ -3,41 +3,7 @@ import Captcha from '../Captcha'
 
 import ContactForm from '../ContactForm/ContactForm'
 function Callback() {
-	useEffect(() => {
-		const input_tel = document.querySelector('#tel')
-		input_tel.addEventListener('input', mask, false)
-		input_tel.addEventListener('focus', mask, false)
-		input_tel.addEventListener('blur', mask, false)
-		function setCursorPosition(pos, elem) {
-			elem.focus()
-			if (elem.setSelectionRange) elem.setSelectionRange(pos, pos)
-			else if (elem.createTextRange) {
-				var range = elem.createTextRange()
-				range.collapse(true)
-				range.moveEnd('character', pos)
-				range.moveStart('character', pos)
-				range.select()
-			}
-		}
 
-		function mask(event) {
-			let matrix = '+7 (___) ___-__-__',
-				i = 0,
-				def = matrix.replace(/\D/g, ''),
-				val = this.value.replace(/\D/g, '')
-			if (def.length >= val.length) val = def
-			this.value = matrix.replace(/./g, function (a) {
-				return /[_\d]/.test(a) && i < val.length
-					? val.charAt(i++)
-					: i >= val.length
-					? ''
-					: a
-			})
-			if (event.type == 'blur') {
-				if (this.value.length == 2) this.value = ''
-			} else setCursorPosition(this.value.length, this)
-		}
-	})
 
 	return (
 		<section className='main__callback' id='main__callback'>
--- stroyximtorg.client/src/components/ContactForm/ContactForm.jsx
+++ stroyximtorg.client/src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Captcha from '../Captcha';
 import '../CallBack/Callback.scss';
 
@@ -10,7 +10,41 @@ const ContactForm = () => {
     e.preventDefault(); // Предотвращаем перезагрузку страницы
     setIsSubmitted(true); // Устанавливаем состояние в "отправлено"
   };
+  useEffect(() => {
+    const input_tel = document.querySelector('#tel')
+    input_tel.addEventListener('input', mask, false)
+    input_tel.addEventListener('focus', mask, false)
+    input_tel.addEventListener('blur', mask, false)
+    function setCursorPosition(pos, elem) {
+        elem.focus()
+        if (elem.setSelectionRange) elem.setSelectionRange(pos, pos)
+        else if (elem.createTextRange) {
+            var range = elem.createTextRange()
+            range.collapse(true)
+            range.moveEnd('character', pos)
+            range.moveStart('character', pos)
+            range.select()
+        }
+    }
 
+    function mask(event) {
+        let matrix = '+7 (___) ___-__-__',
+            i = 0,
+            def = matrix.replace(/\D/g, ''),
+            val = this.value.replace(/\D/g, '')
+        if (def.length >= val.length) val = def
+        this.value = matrix.replace(/./g, function (a) {
+            return /[_\d]/.test(a) && i < val.length
+                ? val.charAt(i++)
+                : i >= val.length
+                ? ''
+                : a
+        })
+        if (event.type == 'blur') {
+            if (this.value.length == 2) this.value = ''
+        } else setCursorPosition(this.value.length, this)
+    }
+})
   return (
     <form id='contact' action='' method='post' onSubmit={handleSubmit}>
       <p className='main__callback--wrapper--label'>
